ab40819b991c54d34880b49e78b2b417f2482838
Jeff Peng <Jeff.Peng@capco.com>
2019-10-28 00:24:07 -0400

    changed deployment setting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import BrowserRouter from 'react-router-dom/BrowserRouter'
+
 
 import App from './app'
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render((
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
+ ))  

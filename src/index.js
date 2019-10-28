4119b6f34f4873d2b27b4023a848b33b0147914d
Jeff Peng <Jeff.Peng@capco.com>
2019-10-28 00:27:45 -0400

    fix bugs with deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import BrowserRouter from 'react-router-dom/BrowserRouter'
+import {BrowserRouter} from 'react-router-dom'
 
 
 import App from './app'
@@ -9,4 +9,4 @@ ReactDOM.render((
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
- ))  
+ ),  document.querySelector('#root'))  

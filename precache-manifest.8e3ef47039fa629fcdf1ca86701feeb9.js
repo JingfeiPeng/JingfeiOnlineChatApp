6d820eb2b0600e8558a0ef27595afcf271af69da
Jeff Peng <Jeff.Peng@capco.com>
2019-10-28 02:40:48 -0400

    Updates

---
+++ precache-manifest.8e3ef47039fa629fcdf1ca86701feeb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1412be62758fd3ce2a9d658c9d3d3f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "69d2cf81fd80a27c9551",
+    "url": "./static/css/main.b3bbce87.chunk.css"
+  },
+  {
+    "revision": "38952699c9ed508714cf",
+    "url": "./static/js/2.5562c5db.chunk.js"
+  },
+  {
+    "revision": "69d2cf81fd80a27c9551",
+    "url": "./static/js/main.14ded16e.chunk.js"
+  },
+  {
+    "revision": "fcdfc5684ead43fa0b3c",
+    "url": "./static/js/runtime-main.274ab6a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/JingfeiOnlineChatApp/precache-manifest.1b4286d0ad492fcb604a7701d7e383cd.js"
+  "./precache-manifest.8e3ef47039fa629fcdf1ca86701feeb9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/JingfeiOnlineChatApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
